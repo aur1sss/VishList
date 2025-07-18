3ebe3af5a5a8dad0edc3cb7aad1aa4a9f654b302
aur1sss <absolute.aur1sss@gmail.com>
2025-07-18 21:38:57 +0300

    added main page and product model

---
+++ Models/Product.cs
@@ -0,0 +1,9 @@
+namespace VishList.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public int Price { get; set; }
+    public string? Description { get; set; }
+}
